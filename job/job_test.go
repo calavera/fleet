4231fbc15e481ccbf13193a6e4104db6de2c055a
David Calavera <david.calavera@gmail.com>
2014-04-25 10:45:40 -0600

    Fix diff.

--- job/job_test.go
+++ job/job_test.go
@@ -6,22 +6,6 @@ import (
 	"github.com/coreos/fleet/unit"
 )
 
-func TestNewJobNilStateNilPayload(t *testing.T) {
-	j1 := NewJob("ping.service", nil)
-
-	if j1.Name != "ping.service" {
-		t.Fatal("job.Job.Name != 'ping.service'")
-	}
-
-	if j1.State != nil {
-		t.Fatal("job.Job.State != nil")
-	}
-
-	if j1.Payload != nil {
-		t.Fatal("job.Job.Payload != nil")
-	}
-}
-
 func TestNewJob(t *testing.T) {
 	jp1 := NewJobPayload("echo.service", *unit.NewSystemdUnitFile("Echo"))
 	j1 := NewJob("pong.service", *jp1)
